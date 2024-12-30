0d331586849d49857ed2519ecb2179eaea25dd0b
Keeby <cjmusic2002@gmail.com>
2024-12-30 14:34:11 -0600

    Update rva_relative_test.py

--- Python Test Scripts/rva_relative_test.py
+++ Python Test Scripts/rva_relative_test.py
@@ -4,7 +4,7 @@ import rva_relative
 
 def rva_relative_test():
     '''
-    This program uses the data of Example 7.1 to calculate the position,
+    This program uses the data to calculate the position,
     velocity and acceleration of an orbiting chaser B relative to an
     orbiting target A.
 
@@ -22,20 +22,20 @@ def rva_relative_test():
     rA, vA           - inertial position (km) and velocity (km/s) of A
     rB, vB           - inertial position (km) and velocity (km/s) of B
     r                - position (km) of B relative to A in A's
-                        co-moving frame
+                       co-moving frame
     v                - velocity (km/s) of B relative to A in A's
-                        co-moving frame
+                       co-moving frame
     a                - acceleration (km/s^2) of B relative to A in A's
-                        co-moving frame
+                       co-moving frame
 
     User py-function required:   sv_from_coe, rva_relative
     '''
     mu  = 398600.4418
     deg = np.pi/180
 
-    # Input data:
+    #...Input data:
 
-    # Spacecraft A:
+    #   Spacecraft A:
     h_A     = 52059
     e_A     = 0.025724
     i_A     = 60 * deg
@@ -43,7 +43,7 @@ def rva_relative_test():
     omega_A = 30 * deg
     theta_A = 40 * deg
 
-    # Spacecraft B:
+    #   Spacecraft B:
     h_B     = 52362
     e_B     = 0.0072696
     i_B     = 50 * deg
@@ -51,14 +51,17 @@ def rva_relative_test():
     omega_B = 120 * deg
     theta_B = 40 * deg
 
-    # Compute the initial state vectors of A and B using Algorithm 4.5:
+    #...End input data
+
+    #...Compute the initial state vectors of A and B using Algorithm 4.5:
     rA, vA = sv_from_coe.sv_from_coe([h_A, e_A, RAAN_A, i_A, omega_A, theta_A], mu)
     rB, vB = sv_from_coe.sv_from_coe([h_B, e_B, RAAN_B, i_B, omega_B, theta_B], mu)
 
-    # Compute relative position, velocity, and acceleration using Algorithm 7.1:
+    #...Compute relative position, velocity, and acceleration using 
+    #   Algorithm 7.1:
     r, v, a = rva_relative.rva_relative(rA, vA, rB, vB, mu)
 
-    # Output
+    #...Output
     print("\n\n--------------------------------------------------------\n\n")
     print("\nOrbital parameters of spacecraft A:")
     print(f"   angular momentum    = {h_A} (km^2/s)")
