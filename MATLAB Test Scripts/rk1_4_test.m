e6d9b800d7a41d3178645bdc49218b25f74f16e9
Keeby <cjmusic2002@gmail.com>
2024-12-30 15:15:39 -0600

    Update rk1_4_test.m

--- MATLAB Test Scripts/rk1_4_test.m
+++ MATLAB Test Scripts/rk1_4_test.m
@@ -1,4 +1,3 @@
-%
 function rk1_4_test
 % 
 %{
@@ -46,38 +45,38 @@ function rk1_4_test
 clear all; close all; clc
 
 %...Input data:
-m = 1;
-z = 0.03;
+m  = 1;
+z  = 0.03;
 wn = 1;
 Fo = 1;
-w = 0.4*wn;
+w  = 0.4*wn;
 
-x0 = 0;
+x0     = 0;
 x_dot0 = 0;
-f0 = [x0; x_dot0];
+f0     = [x0; x_dot0];
 
-t0 = 0;
-tf = 110;
+t0    = 0;
+tf    = 110;
 tspan = [t0 tf];
 %...End input data
 
 %...Solve using RK1 through RK4, using the same and a larger
 % time step for each method:
 rk = 1;
-h = .01; [t1, f1] = rk1_4(@rates, tspan, f0, h, rk);
-h = 0.1; [t11, f11] = rk1_4(@rates, tspan, f0, h, rk);
+h  = .01; [t1, f1]   = rk1_4(@rates, tspan, f0, h, rk);
+h  = 0.1; [t11, f11] = rk1_4(@rates, tspan, f0, h, rk);
 
 rk = 2;
-h = 0.1; [t2, f2] = rk1_4(@rates, tspan, f0, h, rk);
-h = 0.5; [t21, f21] = rk1_4(@rates, tspan, f0, h, rk);
+h  = 0.1; [t2, f2]   = rk1_4(@rates, tspan, f0, h, rk);
+h  = 0.5; [t21, f21] = rk1_4(@rates, tspan, f0, h, rk);
 
 rk = 3;
-h = 0.5; [t3, f3] = rk1_4(@rates, tspan, f0, h, rk);
-h = 1.0; [t31, f31] = rk1_4(@rates, tspan, f0, h, rk);
+h  = 0.5; [t3, f3]   = rk1_4(@rates, tspan, f0, h, rk);
+h  = 1.0; [t31, f31] = rk1_4(@rates, tspan, f0, h, rk);
 
 rk = 4;
-h = 1.0; [t4, f4] = rk1_4(@rates, tspan, f0, h, rk);
-h = 2.0; [t41, f41] = rk1_4(@rates, tspan, f0, h, rk);
+h  = 1.0; [t4, f4]   = rk1_4(@rates, tspan, f0, h, rk);
+h  = 2.0; [t41, f41] = rk1_4(@rates, tspan, f0, h, rk);
 
 output
 
@@ -101,9 +100,9 @@ function dfdt = rates(t,f)
 %}
 % 
 
-x = f(1);
-Dx = f(2);
-D2x = Fo/m*sin(w*t) - 2*z*wn*Dx - wn^2*x;
+x    = f(1);
+Dx   = f(2);
+D2x  = Fo/m*sin(w*t) - 2*z*wn*Dx - wn^2*x;
 dfdt = [Dx; D2x];
 end %rates
 
